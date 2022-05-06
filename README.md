2ee8295c950967940885b86643f576557dfafa25
Lete114 <me@imlete.cn>
2022-05-06 19:22:26 +0800

    feat: 新增: 自定义hash长度、自定义hash版本key、为相对路径资源注入hash版本

--- README.md
+++ README.md
@@ -22,14 +22,18 @@ npm install hexo-hash --save
 > 这些功能只有你需要自定义的时候才会用到
 > 如果不写配置，则使用默认的配置(会对所有资源生成 hash)
 
-在 hexo 的配置文件中添加配置
+在 hexo 的配置文件中添加配置(以下配置示例为默认配置)
 
 ```yml
 # Hexo-hash
 # https://github.com/Lete114/Hexo-hash
 hash:
-  enable: true                          # 是否启用
-  suffix: [.html, .css, .js, .png]      # 指定生成hash的文件后缀
-  ignoreSuffix: [.json]                 # 指定忽略生成hash的文件
-  ignoreDir: [css]                      # 忽略指定目录内的文件
+  enable: true                              # 是否启用
+  size: 10                                  # 生成的hash长度，最大32
+  version: v                                # 自定义版本号key，如: ?v=6faed3522c
+  relative: false                           # 是否为相对路径的资源注入hash版本
+  injectSuffix: ['.html', '.css', '.js']    # 指定为哪些文件注入hash版本
+  suffix: []                                # 指定生成hash的文件后缀
+  ignoreSuffix: []                          # 指定忽略生成hash的文件
+  ignoreDir: []                             # 忽略指定目录内的文件
 ```
--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const urlFor = require('hexo-util').url_for.bind(hexo)
 
 const defualtOptions = {
   enable: true,
+  size: 10,
+  version: 'v',
+  relative: false,
   injectSuffix: ['.html', '.css', '.js'],
   suffix: false,
   ignoreSuffix: false,
@@ -27,16 +30,29 @@ hexo.on('exit', function () {
   const { config, env, public_dir, log } = this
   // 合并配置
   const hash = Object.assign(defualtOptions, config.hash)
-  const { enable, injectSuffix, suffix, ignoreSuffix, ignoreDir } = hash
+  const {
+    enable,
+    size,
+    version,
+    relative,
+    injectSuffix,
+    suffix,
+    ignoreSuffix,
+    ignoreDir
+  } = hash
 
   // 判断当前执行的命令是否是构建命令，并且查看是否存在public_dir
   const condition = ['g', 'generate'].includes(env.cmd)
   if (!condition || !existsSync(public_dir) || !enable) return
 
+  // 读取需要注入hash版本的文件
   const injectFiles = readAllFile(public_dir, { suffix: injectSuffix })
+  // 读取生成hash版本的文件
   const files = readAllFile(public_dir, { suffix, ignoreSuffix, ignoreDir })
-  const mappingObj = mapping(files, public_dir, urlFor)
-  injectHash(injectFiles, mappingObj)
+  // 生成关系映射对象
+  const mappingObj = mapping(files, public_dir, urlFor, size, relative)
+  // 注入
+  injectHash(injectFiles, mappingObj, version, relative)
 
   const interval = prettyHrtime(process.hrtime(start))
   log.i('Successfully injected hash version in %s', cyan(interval))
