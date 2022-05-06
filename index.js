3e6b04346b699187a497b5fc782311a79f576bea
Lete114 <me@imlete.cn>
2022-05-06 16:33:00 +0800

    feat: 新增对指定文件进行注入，默认: html,css,js

--- index.js
+++ index.js
@@ -14,22 +14,29 @@ const { injectHash, mapping, readAllFile } = require('./lib')
 // 获取完整规范化的 hexo source url 地址
 const urlFor = require('hexo-util').url_for.bind(hexo)
 
-const defualtOptions = { enable: true, suffix: false, ignoreSuffix: false, ignoreDir: false }
+const defualtOptions = {
+  enable: true,
+  injectSuffix: ['.html', '.css', '.js'],
+  suffix: false,
+  ignoreSuffix: false,
+  ignoreDir: false
+}
 
 hexo.on('exit', function () {
   var start = process.hrtime()
   const { config, env, public_dir, log } = this
   // 合并配置
   const hash = Object.assign(defualtOptions, config.hash)
-  const { enable, suffix, ignoreSuffix, ignoreDir } = hash
+  const { enable, injectSuffix, suffix, ignoreSuffix, ignoreDir } = hash
 
   // 判断当前执行的命令是否是构建命令，并且查看是否存在public_dir
   const condition = ['g', 'generate'].includes(env.cmd)
   if (!condition || !existsSync(public_dir) || !enable) return
 
+  const injectFiles = readAllFile(public_dir, { suffix: injectSuffix })
   const files = readAllFile(public_dir, { suffix, ignoreSuffix, ignoreDir })
   const mappingObj = mapping(files, public_dir, urlFor)
-  injectHash(files, mappingObj)
+  injectHash(injectFiles, mappingObj)
 
   const interval = prettyHrtime(process.hrtime(start))
   log.i('Successfully injected hash version in %s', cyan(interval))
