61dd744046dd1a3ab0a229958409d4c4d561fc00
Lete114 <me@imlete.cn>
2022-05-06 15:50:12 +0800

    feat: 添加注入hash所使用的时长

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 'use strict'
 
 const { existsSync } = require('fs')
-
+const { cyan } = require('picocolors')
+const prettyHrtime = require('pretty-hrtime')
 const { injectHash, mapping, readAllFile } = require('./lib')
 
 // 获取完整规范化的 hexo source url 地址
@@ -16,18 +17,20 @@ const urlFor = require('hexo-util').url_for.bind(hexo)
 const defualtOptions = { enable: true, suffix: false, ignoreSuffix: false, ignoreDir: false }
 
 hexo.on('exit', function () {
+  var start = process.hrtime()
   const { config, env, public_dir, log } = this
   // 合并配置
   const hash = Object.assign(defualtOptions, config.hash)
   const { enable, suffix, ignoreSuffix, ignoreDir } = hash
 
   // 判断当前执行的命令是否是构建命令，并且查看是否存在public_dir
-  const condition = ['g', 'generate'].includes(env.cmd) 
+  const condition = ['g', 'generate'].includes(env.cmd)
   if (!condition || !existsSync(public_dir) || !enable) return
 
   const files = readAllFile(public_dir, { suffix, ignoreSuffix, ignoreDir })
   const mappingObj = mapping(files, public_dir, urlFor)
   injectHash(files, mappingObj)
 
-  log.i('Successfully injected hash version')
+  const interval = prettyHrtime(process.hrtime(start))
+  log.i('Successfully injected hash version in %s', cyan(interval))
 })
